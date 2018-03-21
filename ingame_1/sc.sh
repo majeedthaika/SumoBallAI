37df848a820da202b85b7b16c3bfb913fce4b9c7
Mohit Singh <mdsingh2013@gmail.com>
2018-03-21 00:11:30 -0400

    images

---
+++ ingame_1/sc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+shopt -s extglob
+shopt -s nocaseglob
+
+IMAGE_TYPES=''
+IFS=$'\n' dirlist=(`find "$PWD" -type d`)
+
+counter=0
+for dir in "${dirlist[@]}"; do
+    cd "$dir"
+    ls *.+($IMAGE_TYPES) > /dev/null 2>&1 || continue
+
+    for file in *.+($IMAGE_TYPES); do
+        printf -v newname "../%.3d.%s" $((counter += 1)) "npy"
+	#printf "$newname"
+        mv --verbose "$file" "$newname"
+    done
+done
